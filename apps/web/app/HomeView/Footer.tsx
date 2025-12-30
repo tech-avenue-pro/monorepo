a70732a4ef09eac300cce8f201e42ce009abd90a
Ali Zafar <ali.zaffar@gmail.com>
2025-12-29 20:16:57 -0500

    Spacing tweaks

--- apps/web/app/HomeView/Footer.tsx
+++ apps/web/app/HomeView/Footer.tsx
@@ -34,7 +34,7 @@ export default function Footer({
                         </DSText>
                     </div>
 
-                    <div className="grid grid-cols-3  gap-4 ">
+                    <div className="grid grid-cols-2 md:grid-cols-3 gap-4 gap-y-12">
                         <div className="flex flex-col gap-4">
                             <DSText
                                 variant={DSTextVariant.headline3}
