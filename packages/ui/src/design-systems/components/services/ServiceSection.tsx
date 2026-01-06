e91d2ff40fdc5340c3d028d2eb74270c66df1b21
Ali Zafar <ali.zaffar@gmail.com>
2026-01-06 10:48:05 -0800

    reverting other animations

--- packages/ui/src/design-systems/components/services/ServiceSection.tsx
+++ packages/ui/src/design-systems/components/services/ServiceSection.tsx
@@ -53,7 +53,7 @@ export default function ServiceSection({
                 </div>
             </div>
 
-            <div className="service-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-16">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-16">
                 {services.map((service) => {
                     const href = service.slug
                         ? `/services/${service.slug}`
