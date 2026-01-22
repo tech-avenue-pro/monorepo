fb566e95626ffe5d514b4c846adfec82b47143ca
Ali Zafar <ali.zaffar@gmail.com>
2026-01-22 12:16:02 -0500

    Animations added for Digital Marketing

--- packages/ui/src/design-systems/components/services/ServiceDetails.tsx
+++ packages/ui/src/design-systems/components/services/ServiceDetails.tsx
@@ -67,13 +67,17 @@ export function ServiceDetails({
         <main>
             {/* HERO */}
             <HeroSection
-                background={DSLayoutBackground.defaultVariation2}
-                className="lg:h-screen"
+                background={DSLayoutBackground.default}
+                className="lg:h-screen hero-glow"
             >
                 <HeroContainer className="h-full">
                     <div className="grid gap-10 lg:grid-cols-2 md:items-center h-full">
                         <div className="py-16">
-                            <DSMotion variant="fade-up" delay={0.5}>
+                            <DSMotion
+                                variant="fade-up"
+                                delay={0.3}
+                                duration={0.3}
+                            >
                                 <DSText
                                     as="h1"
                                     variant={DSTextVariant.largestTitle}
@@ -84,7 +88,11 @@ export function ServiceDetails({
                                     {service.hero.title}
                                 </DSText>
                             </DSMotion>
-                            <DSMotion variant="slide-left" delay={0.7}>
+                            <DSMotion
+                                variant="slide-left"
+                                delay={0.3}
+                                duration={0.3}
+                            >
                                 <DSText
                                     as="p"
                                     variant={DSTextVariant.body}
@@ -94,28 +102,33 @@ export function ServiceDetails({
                                     {service.hero.summary}
                                 </DSText>
                             </DSMotion>
+                            <DSMotion
+                                variant="scale-in"
+                                delay={1}
+                                duration={0.5}
+                            >
+                                <div className="flex flex-col gap-4  sm:flex-row mt-16 ">
+                                    <DSLinkButton
+                                        variant={DSButtonVariant.primary}
+                                        href={service.hero.primaryCta.href}
+                                    >
+                                        {service.hero.primaryCta.label}
+                                    </DSLinkButton>
 
-                            <div className="flex flex-col gap-4  sm:flex-row mt-16 ">
-                                <DSLinkButton
-                                    variant={DSButtonVariant.primary}
-                                    href={service.hero.primaryCta.href}
-                                >
-                                    {service.hero.primaryCta.label}
-                                </DSLinkButton>
-
-                                <DSLinkButton
-                                    variant={DSButtonVariant.contrast}
-                                    href={service.hero.secondaryCta.href}
-                                    className="inline-flex items-center justify-center rounded-xl border border-neutral-200 bg-white px-6 py-3 text-sm font-medium text-neutral-900 hover:bg-neutral-50"
-                                >
-                                    {service.hero.secondaryCta.label}
-                                </DSLinkButton>
-                            </div>
+                                    <DSLinkButton
+                                        variant={DSButtonVariant.contrast}
+                                        href={service.hero.secondaryCta.href}
+                                        className="inline-flex items-center justify-center rounded-xl border border-neutral-200 bg-white px-6 py-3 text-sm font-medium text-neutral-900 hover:bg-neutral-50"
+                                    >
+                                        {service.hero.secondaryCta.label}
+                                    </DSLinkButton>
+                                </div>
+                            </DSMotion>
                         </div>
                         <DSMotion
                             variant="slide-left"
                             delay={0.5}
-                            duration={2}
+                            duration={1}
                             className="h-full"
                         >
                             <div className="hidden lg:block relative h-full overflow-hidden rounded-3xl  p-2">
@@ -145,17 +158,19 @@ export function ServiceDetails({
             <RegularSection>
                 <RegularContainer>
                     <div className="">
-                        <DSText
-                            as="h2"
-                            variant={DSTextVariant.largeTitle}
-                            color={DSTextColor.primary}
-                            className="text-left"
-                            fontStyle={FontStyle.Heading}
-                        >
-                            {service.howItWorks.title}
-                        </DSText>
-
-                        <div className="mt-8 grid gap-5 md:grid-cols-2 lg:grid-cols-3">
+                        <DSMotion variant="fade-up" delay={0.3} duration={0.5}>
+                            <DSText
+                                as="h2"
+                                variant={DSTextVariant.largeTitle}
+                                color={DSTextColor.primary}
+                                weight={DSTextWeight.bold}
+                                className="text-left"
+                                fontStyle={FontStyle.Heading}
+                            >
+                                {service.howItWorks.title}
+                            </DSText>
+                        </DSMotion>
+                        <div className="mt-14 grid gap-5 md:grid-cols-2 lg:grid-cols-3">
                             {service.howItWorks.steps.map((step) => (
                                 <div
                                     key={step.number}
@@ -196,29 +211,42 @@ export function ServiceDetails({
                 </RegularContainer>
             </RegularSection>
             {/* Stats and Data */}
-            <RegularSection>
+            <RegularSection background={DSLayoutBackground.defaultVariation1}>
                 <RegularContainer>
                     <div className="max-w-2xl">
-                        <DSText
-                            as="h2"
-                            variant={DSTextVariant.largeTitle}
-                            weight={DSTextWeight.semiBold}
-                            color={DSTextColor.primary}
-                            fontStyle={FontStyle.Heading}
-                            className=""
+                        <DSMotion
+                            variant="slide-right"
+                            delay={0.3}
+                            duration={0.5}
                         >
-                            {service.stats.title}
-                        </DSText>
-                        <DSText
-                            as="p"
-                            variant={DSTextVariant.body}
-                            weight={DSTextWeight.light}
-                            color={DSTextColor.tertiary}
-                            fontStyle={FontStyle.Body}
-                            className="mt-6"
+                            <DSText
+                                as="h2"
+                                variant={DSTextVariant.largeTitle}
+                                weight={DSTextWeight.bold}
+                                color={DSTextColor.primary}
+                                fontStyle={FontStyle.Heading}
+                                className=""
+                            >
+                                {service.stats.title}
+                            </DSText>
+                        </DSMotion>
+
+                        <DSMotion
+                            variant="slide-left"
+                            delay={0.3}
+                            duration={0.5}
                         >
-                            {service.stats.description}
-                        </DSText>
+                            <DSText
+                                as="p"
+                                variant={DSTextVariant.body}
+                                weight={DSTextWeight.light}
+                                color={DSTextColor.tertiary}
+                                fontStyle={FontStyle.Body}
+                                className="mt-6"
+                            >
+                                {service.stats.description}
+                            </DSText>
+                        </DSMotion>
                     </div>
                     <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 text-gray-900 sm:mt-20 sm:grid-cols-2 sm:gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-4 dark:text-white">
                         {service.stats.items.map((stat) => (
@@ -249,48 +277,54 @@ export function ServiceDetails({
             <RegularSection>
                 <RegularContainer>
                     <div className="rounded-3xl border border-neutral-200 p-12 shadow-sm ds-bg-light-right">
-                        <DSText
-                            as="h2"
-                            variant={DSTextVariant.largeTitle}
-                            weight={DSTextWeight.medium}
-                            color={DSTextColor.primary}
-                            fontStyle={FontStyle.Heading}
-                        >
-                            <span
-                                dangerouslySetInnerHTML={{
-                                    __html: service.whyItWorks.title,
-                                }}
-                            />
-                        </DSText>
-                        <DSText
-                            as="p"
-                            variant={DSTextVariant.body}
-                            weight={DSTextWeight.light}
-                            color={DSTextColor.tertiary}
-                            fontStyle={FontStyle.Body}
-                            className="mt-6 max-w-2xl text-left"
-                        >
-                            <span
-                                dangerouslySetInnerHTML={{
-                                    __html: service.whyItWorks.description,
-                                }}
-                            />
-                        </DSText>
+                        <DSMotion variant="fade-up" delay={0.3} duration={0.3}>
+                            <DSText
+                                as="h2"
+                                variant={DSTextVariant.largeTitle}
+                                weight={DSTextWeight.bold}
+                                color={DSTextColor.primary}
+                                fontStyle={FontStyle.Heading}
+                            >
+                                <span
+                                    dangerouslySetInnerHTML={{
+                                        __html: service.whyItWorks.title,
+                                    }}
+                                />
+                            </DSText>
+                        </DSMotion>
 
-                        <div className="mt-8 flex flex-col gap-4">
-                            {service.whyItWorks.items.map((item) => (
-                                <li>
-                                    <DSText
-                                        as="p"
-                                        variant={DSTextVariant.body}
-                                        color={DSTextColor.secondary}
-                                        fontStyle={FontStyle.Body}
-                                    >
-                                        {item}
-                                    </DSText>
-                                </li>
-                            ))}
-                        </div>
+                        <DSMotion variant="fade-in" delay={0.5} duration={0.5}>
+                            <DSText
+                                as="p"
+                                variant={DSTextVariant.body}
+                                weight={DSTextWeight.light}
+                                color={DSTextColor.tertiary}
+                                fontStyle={FontStyle.Body}
+                                className="mt-6 max-w-2xl text-left"
+                            >
+                                <span
+                                    dangerouslySetInnerHTML={{
+                                        __html: service.whyItWorks.description,
+                                    }}
+                                />
+                            </DSText>
+                        </DSMotion>
+                        <DSMotion variant="slide-left" delay={0.7} duration={1}>
+                            <div className="mt-8 flex flex-col gap-4">
+                                {service.whyItWorks.items.map((item) => (
+                                    <li>
+                                        <DSText
+                                            as="p"
+                                            variant={DSTextVariant.body}
+                                            color={DSTextColor.secondary}
+                                            fontStyle={FontStyle.Body}
+                                        >
+                                            {item}
+                                        </DSText>
+                                    </li>
+                                ))}
+                            </div>
+                        </DSMotion>
                     </div>
                 </RegularContainer>
             </RegularSection>
