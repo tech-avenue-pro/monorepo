26f8fb64b89c775da4add553a19f9ecc128f3e82
Ali Zafar <ali.zaffar@gmail.com>
2026-04-12 12:57:17 -0400

    Gerelaized how we help section

---
+++ packages/ui/src/design-systems/common-content-mappers/service-section-content.ts
@@ -0,0 +1,6 @@
+import { ServiceCardTopNumberContent } from "../components/services/service-card-top-number/service-card-top-number-content";
+
+export interface ServiceSectionTopNumberedContent {
+    title: string;
+    steps: ServiceCardTopNumberContent[];
+}
--- packages/ui/src/design-systems/components/how-we-help/how-we-help-content.ts
+++
@@ -1,6 +0,0 @@
-import { ServiceCardTopNumberContent } from "../services/service-card-top-number/service-card-top-number-content";
-
-export interface HowWeHelpContent {
-    title: string;
-    steps: ServiceCardTopNumberContent[];
-}
